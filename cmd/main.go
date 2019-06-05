21e70cfacc00815eed1b14393eba9f39743a2e44
Jose Moreno <jomoespe@gmail.com>
2019-06-06 00:20:45 +0200

    Add license header

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,7 @@
+// Copyright 2018 Jomoespe. All rights reserved.
+// Use of this source code is governed by a The Unlicense license that can be 
+// found in the LICENSE file.
+
 package main
 
 import "fmt"
